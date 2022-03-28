c2e95b49a4feba71e09b999f95cb63d5f2bb22a9
T <t.kawauchi@4nonome.com>
2022-03-28 23:00:08 +0900

    Updata 07_D.py

--- AOJ/07/07_D.py
+++ AOJ/07/07_D.py
@@ -8,29 +8,33 @@ Created on Mon Mar 28 22:14:22 2022
 n, m, l = map(int, input().split())
 A = []
 B = []
+#行列Aの作成
 for i in range(n):
     a = input().split()
     a1 = []
     for j in range(m):
         a1.append(int(a[j]))
     A.append(a1)
+#行列Bの作成
 for j in range(m):
     b = input().split()
     b1 = []
     for j in range(l):
         b1.append(int(b[j]))
     B.append(b1)
-R = []
+C = []
+#行列の積を計算
 for i in range(n):
-    h = []
+    R = []
     for k in range(l):
         r = 0
         for j in range(m):
             r += A[i][j] * B[j][k]
-        h.append(r)
-    R.append(h)    
-for i in range(len(R)):
-    print(*R[i])
+        R.append(r)
+    C.append(R)
+#結果を出力    
+for i in range(len(C)):
+    print(*C[i])
 
 
            
