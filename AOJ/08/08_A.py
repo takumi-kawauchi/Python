cdf2892d0c6a4640e9fc468eb2ad4daebefe0c36
takumi <88089834+takumi-kawauchi@users.noreply.github.com>
2022-03-29 22:05:07 +0900

    Update 08_A.py

--- AOJ/08/08_A.py
+++ AOJ/08/08_A.py
@@ -1,17 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 28 23:36:13 2022
-
-@author: tayun
-"""
-
 l = []
 l = input().split(', ')
 for i in range(len(l)):
     l[i] = l[i].swapcase()
 print(*l, sep=", ")
-
-
-
-    
     
