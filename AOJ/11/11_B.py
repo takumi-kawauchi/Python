cf27207ab2ddafd25cfb86821e483b7fd11f36e7
T <t.kawauchi@4nonome.com>
2022-04-16 23:05:45 +0900

    Create 11_C.py

--- AOJ/11/11_B.py
+++ AOJ/11/11_B.py
@@ -14,23 +14,40 @@ class Dice:
         self.right = num_list[2]
         self.left = num_list[3]
         self.back = num_list[4]
-        self.bottom = num_list[5]        
+        self.bottom = num_list[5]
+                
     
-    #命令通りにサイコロを転がすための関数
-    def orders(self, order):
-        if order == "E" :
-            self.top, self.left, self.bottom, self.right = \
-            self.left, self.bottom, self.right, self.top
-        if order == "S":
-            self.top, self.front, self.bottom, self.back = \
-            self.back, self.top, self.front, self.bottom
-        if order == "W":
-            self.top, self.left, self.bottom, self.right = \
-            self.right, self.top, self.left, self.bottom
-        if order == "N":
-            self.top, self.front, self.bottom, self.back = \
-            self.front, self.bottom, self.back, self.top
+    #全ての転がし方を試すための関数
+    def orders(self, num_top, num_front):
+        #全通り試すことのできる命令のリストを作成
+        for order in list("RRRRSRRRRSRRRRSRRRRSERRRRERRRRERRRR"):
+            if order == "E" :
+                self.top, self.left, self.bottom, self.right = \
+                self.left, self.bottom, self.right, self.top
+                #指定された上面、前面の値と一致したら、その時に右面を返す。
+                if self.top == num_top and self.front == num_front:
+                    return self.right 
+            if order == "S":
+                self.top, self.front, self.bottom, self.back = \
+                self.back, self.top, self.front, self.bottom
+                if self.top == num_top and self.front == num_front:
+                    return self.right
+            if order == "R":
+                self.right, self.front, self.left, self.back = \
+                self.back, self.right, self.front, self.left
+                if self.top == num_top and self.front == num_front:
+                    return self.right 
     
-    #転がし終わった後、上面の数字を取得する関数
-    def show_top(self):
-        return self.top
+num_list = list(map(int, input().split()))
+order_num = int(input())
+#複数の命令に対応できるように結果を格納するためのリストを用意。
+result = []
+dice = Dice(num_list)
+for _ in range(order_num):
+    num_top, num_front = map(int, input().split())
+    result.append(dice.orders(num_top, num_front))
+for _ in result:
+    print(_)
+
+
+
